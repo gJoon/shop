562874f25329a245163148d424b3ae17056abb47
Your Name <gjoon9613@naver.com>
2022-03-26 00:01:28 +0900

    상품등록

--- product/product_edit.php
+++ product/product_edit.php
@@ -12,72 +12,82 @@ include_once('../include/top.php');
      <div class="bg-white py-5 px-6">
         <form name="form" method="post" enctype="multipart/form-data" action="product_proc.php">
 
-            <div class="">
-              <label for="category" class="block text-sm font-semibold text-[#C65D7B] after:content-['*'] after:ml-0.5 after:text-[#C65D7B]'">대분류</label>
-              <div class="mt-1">
-                   <select name="category" id="category" class="form-select w-full lg:w-2/5 px-3 py-3 mt-2 lg:mt-0 mx-0 lg:mx-1 lg:ml-0 text-[#C65D7B] ml-0 mx-auto bg-white border shadow-sm border-slate-300 placeholder:font-light font-semibold focus:outline-none focus:border-[#C65D7B] focus:ring-[#C65D7B] rounded-md sm:text-sm focus:ring-1 invalid:border-[#C65D7B] invalid:text-[#C65D7B] focus:invalid:border-[#C65D7B] focus:invalid:ring-[#C65D7B] disabled:shadow-none" aria-label="Default select example">
-                        <option value='' selected>대분류</option>
-                    </select>
-                    
+            <div class="flex flex flex-col lg:flex-row">
+              <div class="w-full lg:w-3/4 px-4 mt-2">
+                <label for="category" class="block text-sm font-semibold text-[#C65D7B] after:content-['*'] after:ml-0.5 after:text-[#C65D7B]'">대분류</label>
+                <div class="mt-1">
+                    <select name="category" id="category" class="form-select w-full  px-3 py-3 mt-2 lg:mt-0 mx-0 lg:mx-1 lg:ml-0 text-[#C65D7B] ml-0 mx-auto bg-white border shadow-sm border-slate-300 placeholder:font-light font-semibold focus:outline-none focus:border-[#C65D7B] focus:ring-[#C65D7B] rounded-md sm:text-sm focus:ring-1 invalid:border-[#C65D7B] invalid:text-[#C65D7B] focus:invalid:border-[#C65D7B] focus:invalid:ring-[#C65D7B] disabled:shadow-none" aria-label="Default select example">
+                          <option value=''>대분류</option>
+                          
+                      </select>
+                      
+                  </div>
                 </div>
-            </div>
-            <div class="">
-              <label for="category_sub" class="block text-sm font-semibold text-[#C65D7B] after:content-['*'] after:ml-0.5 after:text-[#C65D7B]'">소분류</label>
-              <div class="mt-1">
-                  <select name="category_sub" id="category_sub" class="form-select w-full lg:w-2/5 px-3 py-3 mt-2 lg:mt-0 mx-0 lg:mx-1 lg:ml-0 text-[#C65D7B] ml-0 mx-auto bg-white border shadow-sm border-slate-300 placeholder:font-light font-semibold focus:outline-none focus:border-[#C65D7B] focus:ring-[#C65D7B] rounded-md sm:text-sm focus:ring-1 invalid:border-[#C65D7B] invalid:text-[#C65D7B] focus:invalid:border-[#C65D7B] focus:invalid:ring-[#C65D7B] disabled:shadow-none" aria-label="Default select example">
-                        <option value='' selected>소분류</option>
-                    </select>
-                    
-                </div>
-            </div>
-            <div class="">
-              <label for="item_title" class="block text-sm font-semibold text-[#C65D7B] after:content-['*'] after:ml-0.5 after:text-[#C65D7B]'">상품명</label>
-              <div class="mt-1">
-                <input type="text" name="item_title" id="item_title"
-                    class="px-3 py-3 text-[#C65D7B] bg-white border shadow-sm border-slate-300 placeholder:font-light font-semibold focus:outline-none focus:border-[#C65D7B] focus:ring-[#C65D7B] block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-[#C65D7B] invalid:text-[#C65D7B] focus:invalid:border-[#C65D7B] focus:invalid:ring-[#C65D7B] disabled:shadow-none"
-                    value="" placeholder="상품명을 입력하세요."> 
-                    
-                </div>
-            </div>
-            <div class="">
-              <label for="item_price" class="block text-sm font-semibold text-[#C65D7B] after:content-['*'] after:ml-0.5 after:text-[#C65D7B]'">상품가격</label>
-              <div class="mt-1">
-                <input type="text" name="item_price" id="item_price"
-                    class="px-3 py-3 text-[#C65D7B] bg-white border shadow-sm border-slate-300 placeholder:font-light font-semibold focus:outline-none focus:border-[#C65D7B] focus:ring-[#C65D7B] block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-[#C65D7B] invalid:text-[#C65D7B] focus:invalid:border-[#C65D7B] focus:invalid:ring-[#C65D7B] disabled:shadow-none"
-                    value="" placeholder="상품가격"> 
-                </div>
-            </div>
-            <div class="">
-              <label for="item_per" class="block text-sm font-semibold text-[#C65D7B] after:content-['*'] after:ml-0.5 after:text-[#C65D7B]'">할인률</label>
-              <div class="mt-1">
-                <input type="text" name="item_per" id="item_per"
-                    class="px-3 py-3 text-[#C65D7B] bg-white border shadow-sm border-slate-300 placeholder:font-light font-semibold focus:outline-none focus:border-[#C65D7B] focus:ring-[#C65D7B] block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-[#C65D7B] invalid:text-[#C65D7B] focus:invalid:border-[#C65D7B] focus:invalid:ring-[#C65D7B] disabled:shadow-none"
-                    value="" placeholder="할인률">
+                <div class="w-full lg:w-3/4 px-4 mt-2">
+                  <label for="category_sub" class="block text-sm font-semibold text-[#C65D7B] after:content-['*'] after:ml-0.5 after:text-[#C65D7B]'">소분류</label>
+                  <div class="mt-1">
+                      <select name="category_sub" id="category_sub" class="form-select w-full px-3 py-3 mt-2 lg:mt-0 mx-0 lg:mx-1 lg:ml-0 text-[#C65D7B] ml-0 mx-auto bg-white border shadow-sm border-slate-300 placeholder:font-light font-semibold focus:outline-none focus:border-[#C65D7B] focus:ring-[#C65D7B] rounded-md sm:text-sm focus:ring-1 invalid:border-[#C65D7B] invalid:text-[#C65D7B] focus:invalid:border-[#C65D7B] focus:invalid:ring-[#C65D7B] disabled:shadow-none" aria-label="Default select example">
+                            <option value=''>소분류</option>
+                        </select>
+                        
+                    </div>
                 </div>
             </div>
 
-            <div class="">
-              <label for="item_image" class="block text-sm font-semibold text-[#C65D7B] after:content-['*'] after:ml-0.5 after:text-[#C65D7B]'">아이템 메인이미지</label>
-              <div class="mt-1 flex">
-                <label class="block">
-                <input type="file" name="item_image" id="item_image" class="block w-full text-sm text-slate-500
-                  file:mr-4 file:py-2 file:px-4
-                  file:rounded-full file:border-0
-                  file:text-sm file:font-semibold
-                  file:bg-violet-50 file:text-violet-700
-                  hover:file:bg-violet-100
-                "/>
-              </label>
+
+            <div class="flex flex flex-col lg:flex-row my-2">
+                <div class="w-full lg:w-3/4 px-4 mt-2">
+                  <label for="item_title" class="block text-sm font-semibold text-[#C65D7B] after:content-['*'] after:ml-0.5 after:text-[#C65D7B]'">상품명</label>
+                  <div class="mt-1">
+                    <input type="text" name="item_title" id="item_title"
+                        class="px-3 py-3 text-[#C65D7B] bg-white border shadow-sm border-slate-300 placeholder:font-light font-semibold focus:outline-none focus:border-[#C65D7B] focus:ring-[#C65D7B] block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-[#C65D7B] invalid:text-[#C65D7B] focus:invalid:border-[#C65D7B] focus:invalid:ring-[#C65D7B] disabled:shadow-none"
+                        value="" placeholder="상품명을 입력하세요."> 
+                        
+                  </div>
+                </div>
+                <div class="w-full lg:w-3/4 px-4 mt-2">
+                  <label for="item_price" class="block text-sm font-semibold text-[#C65D7B] after:content-['*'] after:ml-0.5 after:text-[#C65D7B]'">상품가격</label>
+                  <div class="mt-1">
+                    <input type="text" name="item_price" id="item_price"
+                        class="px-3 py-3 text-[#C65D7B] bg-white border shadow-sm border-slate-300 placeholder:font-light font-semibold focus:outline-none focus:border-[#C65D7B] focus:ring-[#C65D7B] block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-[#C65D7B] invalid:text-[#C65D7B] focus:invalid:border-[#C65D7B] focus:invalid:ring-[#C65D7B] disabled:shadow-none"
+                        value="" placeholder="20000"> 
+                  </div>
+              
+                </div>
               </div>
-            </div>
-       
 
-            <div class="">
-              <label for="item_content" class="block text-sm font-semibold text-[#C65D7B] after:content-['*'] after:ml-0.5 after:text-[#C65D7B]'">상품설명</label>
-              <div class="mt-1">
-                    <textarea name="item_content" id="item_content" cols="30" rows="10">1234</textarea>
+
+
+              <div class="flex flex flex-col lg:flex-row my-2">
+                <div class="w-full lg:w-3/4 px-4 mt-2">
+                  <label for="item_per" class="block text-sm font-semibold text-[#C65D7B] after:content-['*'] after:ml-0.5 after:text-[#C65D7B]'">할인률</label>
+                  <div class="mt-1">
+                    <input type="text" name="item_per" id="item_per"
+                        class="px-3 py-3 text-[#C65D7B] bg-white border shadow-sm border-slate-300 placeholder:font-light font-semibold focus:outline-none focus:border-[#C65D7B] focus:ring-[#C65D7B] block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-[#C65D7B] invalid:text-[#C65D7B] focus:invalid:border-[#C65D7B] focus:invalid:ring-[#C65D7B] disabled:shadow-none"
+                        value="" placeholder="할인률 설정"> 
+                  </div>
                 </div>
-            </div>
+                <div class="w-full lg:w-3/4 px-4 mt-2">
+                <label for="item_image" class="block text-sm font-semibold text-[#C65D7B] after:content-['*'] after:ml-0.5 after:text-[#C65D7B]'">아이템 메인이미지</label>
+                <div class="mt-1">
+                  <label class="block">
+                  <input type="file" name="item_image" id="item_image" class="block w-full text-sm text-slate-500
+                    file:mr-4 file:py-2 file:px-4
+                    file:rounded-full file:border-0
+                    file:text-sm file:font-semibold
+                    file:bg-violet-50 file:text-violet-700
+                    hover:file:bg-violet-100
+                  "/>
+                  </label>
+                </div>
+              
+                </div>
+              </div>
+            
+
+
+
+
 
             <div class="mt-6 text-right">
               <button type="button" id="submit_btn" onclick="check();" class="w-full border-[#C65D7B] font-semibold border text-[#C65D7B] py-3 block  text-center rounded-full hover:bg-[#C65D7B] hover:text-[#ffffff] transition-colors hover:text-white mt-8">
@@ -85,21 +95,17 @@ include_once('../include/top.php');
               </button>
               
             </div>
+           </div>
           </form>
      </div>
-   
-
-        
-        
-
-      
-    </div>
     
 
 </article>
 
 
 <script>
+
+
     document.getElementById('submit_btn').onclick = function() {
         form.submit();		
     };
--- product/product_proc.php
+++ product/product_proc.php
@@ -2,18 +2,37 @@
 include '../session.php';
 include '../db_config.php';
 
+//지금시각
+$today =  time();
 
+//이미지 명 생성
+$namechar  = "0123456789";  
+$lang_loops = 6;  
+while ($lang_loops--)  
+{   
+	$img_name .= $namechar[mt_rand(0, strlen($namechar) - 1)];  
+}  
+$lang_loops2 = 4;  
+while ($lang_loops2--)  
+{   
+	$img_name2 .= $namechar[mt_rand(0, strlen($namechar) - 1)];  
+}  
+	
+$img_name = $img_name."_".$img_name2;
 
 // 임시로 저장된 정보(tmp_name)
 $tempFile = $_FILES['item_image']['tmp_name'];
+
 // 파일타입 및 확장자 체크
 $fileTypeExt = explode("/", $_FILES['item_image']['type']);
 // 파일 타입 
 $fileType = $fileTypeExt[0];
 // 파일 확장자
 $fileExt = $fileTypeExt[1];
+$img_name .= ".".$fileExt;
 // 확장자 검사
 $extStatus = false;
+
 switch($fileExt){
 	case 'jpeg':
 	case 'jpg':
@@ -27,46 +46,59 @@ switch($fileExt){
 		exit;
 		break;
 }
-// 이미지 파일이 맞는지 검사. 
-if($fileType == 'image'){
-	// 허용할 확장자를 jpg, bmp, gif, png로 정함, 그 외에는 업로드 불가
-	if($extStatus){
-		// 임시 파일 옮길 디렉토리 및 파일명
-		$resFile = "img/{$_FILES['item_image']['name']}";
-		// 임시 저장된 파일을 우리가 저장할 디렉토리 및 파일명으로 옮김
-		$imageUpload = move_uploaded_file($tempFile, $resFile);
-		// 업로드 성공 여부 확인
-		if($imageUpload == false){
-			echo "파일이 정상적으로 업로드 되었습니다. <br>";
-			echo "<img src='{$resFile}' width='100' />";
-		}else{
-			echo "파일 업로드에 실패하였습니다.";
-		}
-	}	// end if - extStatus
-		// 확장자가 jpg, bmp, gif, png가 아닌 경우 else문 실행
-	else {
-		echo "파일 확장자는 jpg, bmp, gif, png 이어야 합니다.";
-		exit;
-	}	
-}	// end if - filetype
-	// 파일 타입이 image가 아닌 경우 
-else {
-	echo "이미지 파일이 아닙니다.";
-	exit;
-}
 
 
+//아이템 코드 생성
+$length = 9;
+$characters  = "0123456789";  
+$characters .= "abcdefghijklmnopqrstuvwxyz";  
+$characters .= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  
+$item_code = "";  
+$nmr_loops = $length;  
+while ($nmr_loops--)  
+{   
+	$item_code .= $characters[mt_rand(0, strlen($characters) - 1)];  
+}  
+
+
+$item_image = $img_name;
+$item_code = $_POST[category_sub]."_".$item_code;
 $category = $_POST[category];
 $category_sub = $_POST[category_sub];
 $item_title = $_POST[item_title];
 $item_price = $_POST[item_price];
 $item_per = $_POST[item_per];
 $item_content = $_POST[item_content];
-
-
-$stmt = $DB->prepare("insert into item (category,category_sub,item_title,item_price,item_per,item_content) values (?,?,?,?,?,?)");
-$stmt->bind_param( "ssssss" , $category,$category_sub,$item_title,$item_price,$item_per,$item_content);
+$stmt = $DB->prepare("insert into item (item_code,category,category_sub,item_title,item_price,item_per,item_image,write_time) values (?,?,?,?,?,?,?,?)");
+$stmt->bind_param( "ssssiisi" , $item_code,$category,$category_sub,$item_title,$item_price,$item_per,$item_image,$today);
 $stmt->execute();
+
+// 이미지 파일이 맞는지 검사. 
+if($fileType == 'image'){
+	// 허용할 확장자를 jpg, bmp, gif, png로 정함, 그 외에는 업로드 불가
+	if($extStatus){
+		// 임시 파일 옮길 디렉토리 및 파일명
+		$resFile = "img/{$img_name}";
+		// 임시 저장된 파일을 우리가 저장할 디렉토리 및 파일명으로 옮김
+		$imageUpload = move_uploaded_file($tempFile, $resFile);
+	}
+		// end if - extStatus
+		// 확장자가 jpg, bmp, gif, png가 아닌 경우 else문 실행
+	else {
+		echo "<script>alert('파일 확장자는 jpg, bmp, gif, png 이어야 합니다.');
+		location.href='/'
+		</script>";
+		exit;
+	}	
+}	// end if - filetype
+	// 파일 타입이 image가 아닌 경우 
+else {
+	echo "<script>alert('이미지만 업로드 가능합니다.');
+    location.href='/'
+    </script>";
+	exit;
+}
+
 
 
 echo "<script>alert('작품이 등록되었습니다.');
