f5ff82c3ba7eb15bd366f749afbc5768f5d9caf9
Your Name <gjoon9613@naver.com>
2022-03-31 22:57:21 +0900

    상품등록

--- product/product_edit.php
+++ product/product_edit.php
@@ -109,27 +109,43 @@ include_once('../include/top.php');
 
 
               <div class="flex flex flex-col lg:flex-row my-2">
-                <div class="w-full px-4 mt-2 item_sub">
-                <button type="button" class="btn" onclick="img_add();">이미지 추가</button>
-                      <div id="img_box">
-                     
-                            <input type="file" name="item_sub" class="img_add mt-2 block w-full text-sm text-slate-500
-                              file:mr-4 file:py-2 file:px-4
-                              file:rounded-full file:border-0
-                              file:text-sm file:font-semibold
-                              file:bg-black-50 file:text-black-700
-                              hover:file:bg-black-100
-                            "/>
-                     
-                      </div>
-
-                </div>
+                  <div class="w-full px-4 mt-2 item_sub">
+                  <button type="button" onclick="plus();" class="cursor-pointer border">이미지 추가</button>
+                  
+                  <button type="button" onclick="box_allremove();" class="cursor-pointer border">전체제거</button>
+                  <div id="img_box"></div>           
+                
+                  </div>
               </div>
-            
 
 
 
+              <div class="flex flex flex-col lg:flex-row">
+              <div class="w-full lg:w-2/4 px-4 mt-2">
+                  <label for="option_title" class="block text-sm font-semibold text-[#C65D7B] mb-1">옵션명 </label>
+                           
+              <input type="text" name="option_title" id="option_title"
+                        class="px-3 py-3 text-[#C65D7B] bg-white border shadow-sm border-slate-300 placeholder:font-light font-semibold focus:outline-none focus:border-[#C65D7B] focus:ring-[#C65D7B] block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-[#C65D7B] invalid:text-[#C65D7B] focus:invalid:border-[#C65D7B] focus:invalid:ring-[#C65D7B] disabled:shadow-none"
+                        value="" placeholder="예) 컬러 "> 
+                          </div>
+                <div class="w-full lg:w-2/4 px-4 mt-2">
+                  <label for="option_value" class="block text-sm font-semibold text-[#C65D7B] mb-1">옵션값은 [ , ]로 구분합니다)</label>
+                           
+                  <input type="text" name="option_value" id="option_value"
+                  class="px-3 py-3 text-[#C65D7B] bg-white border shadow-sm border-slate-300 placeholder:font-light font-semibold focus:outline-none focus:border-[#C65D7B] focus:ring-[#C65D7B] block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-[#C65D7B] invalid:text-[#C65D7B] focus:invalid:border-[#C65D7B] focus:invalid:ring-[#C65D7B] disabled:shadow-none"
+                  value="" placeholder="RED,BLUE"> 
+                </div>
+                <div class="w-full lg:w-1/5 px-4"> 
+                           
+                    <button type="button" type="button" onclick="preview_option();" class="py-2 w-full border-[#C65D7B] font-semibold border text-[#C65D7B] block  text-center rounded-sm hover:bg-[#C65D7B] hover:text-[#ffffff] transition-colors hover:text-white my-8">
+                      옵션등록
+                    </button>
+                 </div>
+              </div>
 
+              <div id="option_box">
+
+              </div>
 
             <div class="mt-6 text-right">
               <button type="button" id="submit_btn" onclick="check();" class="w-full border-[#C65D7B] font-semibold border text-[#C65D7B] py-3 block  text-center rounded-full hover:bg-[#C65D7B] hover:text-[#ffffff] transition-colors hover:text-white mt-8">
@@ -193,13 +209,81 @@ include_once('../include/top.php');
       }
    
       //이미지 동적 추가버튼
-      let img_cnt = 1;
-      function img_add (){
-
-        $('.img_add').clone().appendTo('#img_box').prop('name', 'img_sub' + img_cnt);
-        img_cnt++;
-
+      let num = 1;
+      const plus = () => {
+       
+        if(num >= 6){
+          alert('이미지는 5개만 추가 가능하십니다.');
+          return num;
+        }else if(num <= 5){
+          const item = document.querySelector("#img_box");
+          let divItem = document.createElement(`span`);
+          item.appendChild(divItem);
+          divItem.innerHTML=`
+          <input type="file" name="item_sub${num}" id="item_sub${num}" class="mt-2 w-2/3 text-sm text-slate-500
+            file:mr-4 file:py-2 file:px-4
+            file:rounded-full file:border-0
+            file:text-sm file:font-semibold
+            file:bg-black-50 file:text-black-700
+            hover:file:bg-black-100
+          "/><span onclick="box_remove('item_sub${num}');" class="border px-2 rounded-full cursor-pointer">제거</span>
+          `;
+          num ++;
+        };
       }
+        //이미지 동적 전체 제거버튼 
+        const box_allremove = () => {
+          if(num != 1){
+              if(confirm("정말 전체 삭제 하시겠습니까?") == true) {
+              num = 1;
+              let all = document.querySelector("#img_box");
+              all.innerHTML = '';
+            }
+          }else{
+            alert('현재 추가하신 이미지가 없습니다.');
+          }
+ 
+        }
+        //이미지 동적 개인 제거버튼 
+        const box_remove = (name) => {
+          num --;
+          let element = document.getElementById(name);
+          let parent = element.parentNode;
+          element.parentNode.remove(parent);
+          console.log(num);
+        }
+
+
+        // option_title 이 담길 변수 선언
+        let opt_title_arr = [];    
+        // option_value 가 담길 변수 선언 
+        let opt_value_arr = {};    
+        async function preview_option(){
+         
+          opt_title_arr.push(document.querySelector('#option_title').value);
+          let value = document.querySelector('#option_value').value;
+          opt_value_arr[0] = value; 
+          
+          // get 방식으로 보내기 위해 우선 opt_value_arr 을 문자열로 직렬화
+          opt_value_arr = JSON.stringify(opt_value_arr);  
+          // ajax 통신할 url
+          let get_url = `option_ajax.php`;   
+          let request_params = {  
+            opt_title_arr,
+            opt_value_arr,
+          }
+          request_params = new URLSearchParams(request_params).toString();    // get parameter는 url 방식으로 데이터를 보내기 때문에 url 형식으로 request_params 를 변경해줌
+          let get_html = await fetch(`${get_url}?${request_params}`, { // 실제 ajax 통신을 하기 위해 요청하는 코드 (fetch API 사용)
+            headers: {  // json 형태로 보내고 utf-8 인코딩을 해서 보내기 위한 설정
+              'Content-Type': 'application/json; charset=utf-8'
+            },
+          });
+          let data = await get_html.text();   // ajax 통신해서 받은 결과값 promise 를 await로 데이터 값으로 받으면서 text 형식으로 변경해줌
+          document.querySelector(`#option_box`).innerHTML = data;  // 파싱된 값을 구역에 넣어줌
+          opt_title_arr = [];
+          opt_value_arr = {};
+        }
+
 
     document.getElementById('submit_btn').onclick = function() {
         form.submit();		
