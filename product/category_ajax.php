22961c05c7f9292835b8dd5efa1df0927ea7fe59
gJoon <gjoon9613@naver.com>
2022-04-24 01:46:06 +0900

    아이템 수정

--- product/category_ajax.php
+++ product/category_ajax.php
@@ -5,8 +5,8 @@ include '../session.php';
 include '../db_config.php';
 
 
-
-  //카테고리 불러오기
+//깃 오류 자꾸발생
+  //카테고리 불러오기 
   $depth = "1";
   $stmt = $DB->prepare("select * from category where depth=? and bcode =?");
   $stmt->bind_param("is", $depth,$_GET[category_val]);
